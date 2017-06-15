be2cdfa4148a09b3316b39ce9f8f34abe37f2dee
mykhailo.petrenko <mapetrenko@luxoft.com>
2017-06-15 18:16:36 +0300

    Implement logic using ReduceStore

--- src/tasks.js
+++ src/tasks.js
@@ -3,6 +3,26 @@ import {Dispatcher, ReduceStore} from './flux';
 
 const tasksDispatcher = new Dispatcher();
 
+const CREATE_TASK = 'CREATE_TASK';
+const COMPLETE_TASK = 'COMPLETE_TASK';
+const SHOW_TASK = 'SHOW_TASK';
+
+const createNewTaskAction = content => ({
+    type: CREATE_TASK,
+    value: content
+});
+
+const completeTaskAction = (id, isCompleted) => ({
+    type: COMPLETE_TASK,
+    id: id,
+    value: isCompleted
+});
+
+const showTaskAction = show => ({
+    type: SHOW_TASK,
+    value: show
+});
+
 class TasksStore extends ReduceStore {
     getInitialState() {
         return {
@@ -33,7 +53,35 @@ class TasksStore extends ReduceStore {
     }
 
     reduce(state, action) {
-        console.log('Reducing ', state, action);
+        var newState;
+
+        switch(action.type) {
+            case CREATE_TASK:
+                newState = {...state, tasks: [...state.tasks]};
+                newState.tasks.push({
+                    id: generate(),
+                    content: action.value,
+                    completed: false
+                });
+                return newState;
+
+            case SHOW_TASK:
+                newState = {...state, tasks: [...state.tasks]};
+                newState.showCompleted = action.value;
+                return newState;
+
+            case COMPLETE_TASK:
+                newState = {...state, tasks: [...state.tasks]};
+
+                const taskIndex = newState.tasks.findIndex(task => task.id==action.id);
+                const task = newState.tasks[taskIndex];
+                newState.tasks[taskIndex] = {
+                    ...task,
+                    completed: action.value
+                }
+
+                return newState;
+        }
 
         return state;
     }
@@ -43,6 +91,71 @@ class TasksStore extends ReduceStore {
     }
 }
 
-const tasksStore = new TasksStore(tasksDispatcher);
+const TaskComponent = ({content, completed, id}) => (`
+    <section>
+        ${content} <input
+                        type="checkbox"
+                        name="taskCompleteCheckbox"
+                        data-taskid="${id}"
+                        ${completed ? 'checked="checked"' : ''}
+                    />
+    </section>
+`);
 
-tasksDispatcher.dispatch('TEST_ACTION');
+const render = () => {
+    const tasksSection = document.getElementById('tasks');
+    const state = tasksStore.getState();
+    const rendered = state
+        .tasks
+        .filter(task=>state.showCompleted || !task.completed)
+        .map(TaskComponent)
+        .join('');
+
+    tasksSection.innerHTML = rendered;
+
+    document.getElementsByName('taskCompleteCheckbox').forEach(element => {
+        element.addEventListener('change', onTaskCompleteChange);
+    });
+};
+
+document.forms.newTask.addEventListener('submit', onNewTaskSubmit);
+document.getElementById('showComplete').addEventListener('change', onShowCompleteChange);
+document.forms.undo.addEventListener('submit', onUndoSubmit);
+
+const tasksStore = new TasksStore(tasksDispatcher);
+tasksStore.addListener(render);
+render();
+
+function onNewTaskSubmit(e) {
+    e.preventDefault();
+    
+    const input = e.target.newTaskName;
+    const name = input.value;
+
+    if (!name) {
+        return;
+    }
+
+    tasksDispatcher.dispatch(createNewTaskAction(name));
+    input.value = '';
+}
+
+function onTaskCompleteChange(e) {
+    e.preventDefault();
+    
+    const id = e.target.attributes['data-taskid'].value;
+    const isCompleted = e.target.checked;
+
+    tasksDispatcher.dispatch(completeTaskAction(id, isCompleted));
+}
+
+function onShowCompleteChange({target}) {
+    const showCompleted = target.checked;
+    tasksDispatcher.dispatch(showTaskAction(showCompleted));
+}
+
+function onUndoSubmit(e) {
+    e.preventDefault();
+
+    tasksStore.revertLastState();
+}
